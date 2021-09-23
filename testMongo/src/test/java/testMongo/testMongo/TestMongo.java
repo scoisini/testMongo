e6319f946a9bd812f7c0179519a012caeafcb7d4
ASUS <ASUS@LAPTOP-LL6KOV8U>
2021-09-23 18:54:45 +0800

    test

---
+++ testMongo/src/test/java/testMongo/testMongo/TestMongo.java
@@ -0,0 +1,20 @@
+package testMongo.testMongo;
+
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class TestMongo {
+	public static void main(String[] args ) {
+	    // Replace the uri string with your MongoDB deployment's connection string
+	    String uri = "mongodb://127.0.0.1:27017";
+	    try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("bencao");
+	        MongoCollection<Document> collection = database.getCollection("tone");
+	        Document doc = collection.find().first();
+	        System.out.println(doc.toJson());
+	    }
+	}
+}
